30fe1c253c3665cc4a5e1f6ef6a6d6e97a58180b
radioaktive <31400136+radioaktive@users.noreply.github.com>
2018-01-17 10:58:22 +0300

    Update webview.js

--- webview.js
+++ webview.js
@@ -1,10 +1,20 @@
 'use strict';
+
+var _path = require('path');
+
+var _path2 = _interopRequireDefault(_path);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 module.exports = Franz => {
   const getMessages = function getMessages() {
-    const mailSel = document.querySelector('.mmi_mail em.mm_counter');
+    const mailSel = document.querySelector('#l_msg span.inl_bl.left_count');
     let count = parseInt(mailSel.innerHTML, 10);
     Franz.setBadge(count);
   };
 
   Franz.loop(getMessages);
+  
+  // inject franz.css stylesheet
+  Franz.injectCSS(_path2.default.join(__dirname, 'service.css'));
 };
